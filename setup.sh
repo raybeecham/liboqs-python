6cc7fc4d2293638ed4cbb834eecceb05bc1f6e2e
Ray Beecham <RayQuantumWave@gmail.com>
2023-12-15 14:51:05 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 cmake -S liboqs-0.9.0 -B liboqs/build -DBUILD_SHARED_LIBS=ON
 cmake --build liboqs/build --parallel 8
 cmake --build liboqs/build --target install
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+sudo export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 . venv/bin/activate
